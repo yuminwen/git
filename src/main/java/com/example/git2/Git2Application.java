7e8ba9c7742ba768aaa6f8b3dc1f4baa278f5159
tom_glb <goodMorning_glb@atguigu.com>
2020-04-09 13:01:58 +0800

    root init

---
+++ src/main/java/com/example/git2/Git2Application.java
@@ -0,0 +1,13 @@
+package com.example.git2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2Application.class, args);
+    }
+
+}
